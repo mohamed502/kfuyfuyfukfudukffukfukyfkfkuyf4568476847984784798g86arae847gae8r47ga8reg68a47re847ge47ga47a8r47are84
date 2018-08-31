d5171179b245346e9ef6c901fe60b101e59769df
mohamed502 <40800726+mohamed502@users.noreply.github.com>
2018-09-01 02:17:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,8 +23,6 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
  const dateFormat = require('dateformat'); 
  const pretty = require('pretty-ms') 
-ti={}  
-spee={};
 
 client.on('message', async message => {//By Codes , - ST I EdiTeD , .#4968
   if(message.content.startsWith(`-تقديم`)) {//By Codes , - ST I EdiTeD , .#4968//By Codes , - ST I EdiTeD , .#4968//By Codes , - ST I EdiTeD , .#4968
